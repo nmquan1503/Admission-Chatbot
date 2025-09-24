c065bc375083bdf52215e0c382d9cdbd3daa28b9
Nguyen Minh Quan <minhquan15032005@gmail.com>
2025-09-24 13:58:26 +0700

    render markdown and setup docker

--- frontend/src/components/MessageItem.tsx
+++ frontend/src/components/MessageItem.tsx
@@ -1,4 +1,5 @@
 import type { Message } from "../types"
+import ReactMarkdown from 'react-markdown'
 
 interface Probs {
     message: Message
@@ -8,11 +9,14 @@ export default function MessageItem({ message }: Probs) {
     const isUser = message.sender === 'user'
     return (
         <div className={`w-full flex ${isUser ? 'justify-end' : 'justify-start'}`}>
-            <div className={`break-words rounded-3xl px-5 py-2 ${
+            <div className={`prose prose-sm break-words rounded-3xl px-5 py-2 ${
                 isUser 
                 ? 'max-w-[90%] max-w-[500px] bg-gray-400 text-white' 
                 : 'max-w-full'
-            }`}>{message.text}</div>
+            }`}
+            >
+                <ReactMarkdown>{message.text}</ReactMarkdown>
+            </div>
         </div>
     );
 }
